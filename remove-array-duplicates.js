dfe1855aa61275f70a947a67e56a171ea8801a45
starkiller17 <eduardo.said.mndz@gmail.com>
2022-04-25 17:07:45 -0500

    remove duplicates from array

---
+++ remove-array-duplicates.js
@@ -0,0 +1,17 @@
+var removeDuplicateNums = (numbers) => {
+  // TODO
+  const array_without_duplicates = [];
+  const numbers_in_array = {};
+
+  numbers.forEach(number => {
+    if (!numbers_in_array[number])
+      array_without_duplicates.push(number);
+    
+      numbers_in_array[number] = number;
+  });
+  
+  console.log(numbers_in_array);
+  return array_without_duplicates;
+};
+
+console.log(removeDuplicateNums([1, 2, 3, 2, 4, 4, 4]));
