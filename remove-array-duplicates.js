fc490edda6b93dc95edf875ec4c621ebbef9d7c1
starkiller17 <eduardo.said.mndz@gmail.com>
2022-05-16 07:55:32 -0500

    fix identation

--- remove-array-duplicates.js
+++ remove-array-duplicates.js
@@ -7,11 +7,11 @@ var removeDuplicateNums = (numbers) => {
     if (!numbers_in_array[number])
       array_without_duplicates.push(number);
     
-      numbers_in_array[number] = number;
+    numbers_in_array[number] = number;
   });
   
   console.log(numbers_in_array);
   return array_without_duplicates;
 };
 
-console.log(removeDuplicateNums([1, 2, 3, 2, 4, 4, 4]));
+console.log(removeDuplicateNums([5, 1, 2, 3, 2, 4, 4, 4, 5, 8]));
